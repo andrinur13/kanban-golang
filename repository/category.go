f60f2b76f1ded2c8a2266b25d06390df395f3f71
andrikuwito <andri.535180062@stu.untar.ac.id>
2022-01-17 22:10:53 +0700

    category
    category

---
+++ repository/category.go
@@ -0,0 +1,82 @@
+package repository
+
+import (
+	"kanban-golang/model/entity"
+
+	"gorm.io/gorm"
+)
+
+type CategoryRepository interface {
+	Save(input entity.Category) (entity.Category, error)
+	CheckType(tipe string) (entity.Category, error)
+	GetByID(ID int) (entity.Category, error)
+	Update(ID int, category entity.Category) (entity.Category, error)
+	Delete(ID int) (entity.Category, error)
+}
+
+type categoryRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepository(db *gorm.DB) *categoryRepository {
+	return &categoryRepository{db}
+}
+
+func (r *categoryRepository) Save(category entity.Category) (entity.Category, error) {
+	err := r.db.Create(&category).Error
+
+	if err != nil {
+		return category, err
+	}
+
+	return category, nil
+}
+
+func (r *categoryRepository) CheckType(tipe string) (entity.Category, error) {
+	typeSame := entity.Category{}
+
+	err := r.db.Where("type = ?", tipe).Find(&typeSame).Error
+
+	if err != nil {
+		return entity.Category{}, err
+	}
+
+	return typeSame, nil
+}
+
+func (r *categoryRepository) GetByID(ID int) (entity.Category, error) {
+	userResult := entity.Category{}
+
+	err := r.db.Where("id = ?", ID).Find(&userResult).Error
+
+	if err != nil {
+		return entity.Category{}, err
+	}
+
+	return userResult, nil
+}
+
+func (r *categoryRepository) Update(ID int, category entity.Category) (entity.Category, error) {
+	err := r.db.Where("id = ?", ID).Updates(&category).Error
+
+	if err != nil {
+		return entity.Category{}, err
+	}
+
+	return category, nil
+}
+
+func (r *categoryRepository) Delete(ID int) (entity.Category, error) {
+
+	Deleted := entity.Category{
+		ID: ID,
+	}
+
+	err := r.db.Where("id = ?", ID).Delete(&Deleted).Error
+
+	if err != nil {
+		return entity.Category{}, err
+	}
+
+	return Deleted, err
+}
